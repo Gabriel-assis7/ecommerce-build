f6d90b1d368042a42bf153020a46b3bcdfd70d55
Gabriel Assis <cardimg06@gmail.com>
2025-03-09 17:33:11 -0300

    feat: set up clerk/sanity

---
+++ src/sanity/env.ts
@@ -0,0 +1,22 @@
+import { env } from "@/data/env/client";
+
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-03-09";
+
+export const dataset = assertValue(
+  env.NEXT_PUBLIC_SANITY_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+);
+
+export const projectId = assertValue(
+  env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
+);
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage);
+  }
+
+  return v;
+}
