9ab7be5ba5bb8bc3c249fd1e2850eb4144a684d3
Gabriel Assis <cardimg06@gmail.com>
2025-03-23 18:56:33 -0300

    feat: add sanity schema

---
+++ src/app/(store)/search/page.tsx
@@ -0,0 +1,10 @@
+export default async function SearchPage({
+  searchParams,
+}: {
+  searchParams: {
+    query: string;
+  };
+}) {
+  const { query } = await searchParams;
+  return <div>Search Page for {query}</div>;
+}
